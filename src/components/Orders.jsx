ca6889ae7e2116af36742d275132a1e21d8cdddd
abhiinandhu2004-beep <abhiinandhu2004@gmail.com>
2025-12-19 16:18:44 +0530

    final

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -15,7 +15,7 @@ export const Orders = ({ cart, setCart, onClose }) => {
       className="
         w-full bg-[#1F1D2B] px-5 py-5 text-white flex flex-col
         lg:fixed lg:top-0 lg:right-0 lg:h-screen lg:w-96 lg:z-50
-        lg:rounded-l-2xl
+        lg:rounded-l-2xl 
       "
     >
       <div className="flex justify-between items-center mb-4 mt-2">
@@ -25,13 +25,19 @@ export const Orders = ({ cart, setCart, onClose }) => {
         </button>
       </div>
 
+      <div className="flex justify-around gap-4 mt-4">
+        <button className="bg-[#1F1D2B] text-[#F99147] hover:bg-[#EA7C69] rounded-2xl px-4 py-2 hover:text-white border border-gray-600 ">Dine In</button>
+        <button className="bg-[#1F1D2B] text-[#F99147] hover:bg-[#EA7C69] rounded-2xl px-4 py-2 hover:text-white border border-gray-600">Take Away</button>
+        <button className="bg-[#1F1D2B] text-[#F99147] hover:bg-[#EA7C69] rounded-2xl px-4 py-2 hover:text-white border border-gray-600">Delivery</button>
+      </div>
+
       <div className="grid grid-cols-6 border-b border-gray-700 pb-2 text-sm mb-3 mt-11">
         <span className="col-span-3">Item</span>
         <span className="text-center">Qty</span>
         <span className="col-span-2 text-right">Price</span>
       </div>
 
-      <div className="flex-1 overflow-y-auto pr-2">
+      <div className="flex-1 overflow-y-auto pr-2 hide-scrollbar">
         {cart.map((item, i) => (
           <div key={i} className="border-b border-gray-700 pb-4 mb-4">
             <div className="grid grid-cols-6 items-center">
@@ -50,7 +56,7 @@ export const Orders = ({ cart, setCart, onClose }) => {
 
             <div className="relative mt-3">
               <input
-                className="w-full bg-[#2C3045] px-3 py-2 rounded-md text-xs"
+                className="w-70 bg-[#2C3045] px-3 py-2 rounded-md text-xs"
                 placeholder="Order Note..."
               />
               <AiOutlineDelete
@@ -62,14 +68,21 @@ export const Orders = ({ cart, setCart, onClose }) => {
         ))}
       </div>
 
+      <div className="pb-11 lg:pb-1">
+        <div className="flex justify-between text-sm text-gray-400 mt-4">
+        <span>Discount</span>
+        <span>5%</span>
+      </div>
+
       <div className="flex justify-between text-sm text-gray-400 mt-4">
         <span>Sub total</span>
         <span>{subtotal.toFixed(2)} AED</span>
       </div>
 
-      <button className="mt-4 bg-[#F99147] py-3 rounded-md">
+      <button className="mt-4 bg-[#F99147] py-3 rounded-md w-full">
         Order Now
       </button>
+      </div>
     </div>
   );
 };
