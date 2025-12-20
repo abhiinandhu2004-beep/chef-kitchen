7a14f56a6dc1a4dd30c448d1626faa6506bf382c
abhiinandhu2004-beep <abhiinandhu2004@gmail.com>
2025-12-20 12:44:45 +0530

    hi

--- src/components/Cheffkitchen.jsx
+++ src/components/Cheffkitchen.jsx
@@ -179,7 +179,7 @@ export const Cheffkitchen = () => {
     <div className="flex flex-col w-full min-h-screen bg-[#2D303E] md:flex-col lg:flex-row ">
       <Sidebar />
       <div className={`
-    flex flex-col lg:flex-row lg:w-full lg:ml-30
+    flex flex-col lg:flex-row lg:w-full lg:ml-30 ml-0
     transition-all duration-300
     ${showOrders ? "lg:mr-100" : "lg:mr-0"}
   `}>
